1e58a37b61a1976f5fbe2a67aa2c3cf16ba410dd
Daniel Vinntreus <gudski@gmail.com>
2012-02-19 23:53:09 +0100

    add home controller to test without raven

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -17,7 +17,7 @@ namespace Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
 
