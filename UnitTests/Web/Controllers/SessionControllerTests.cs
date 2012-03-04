043f4088142fad26bd1decc59bcb4c0a0cb6a2d3
Daniel Vinntreus <gudski@gmail.com>
2012-03-04 20:54:29 +0100

    add session controller

---
+++ UnitTests/Web/Controllers/SessionControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Web.Controllers;
+
+namespace UnitTests.Web.Controllers
+{
+	[TestFixture]
+	public class SessionControllerTests
+	{
+		private SessionController controller;
+
+		[SetUp]
+		public void Setup()
+		{
+			controller = new SessionController();
+		}
+
+		[Test]
+		public void CreateGet_ShouldReturnView()
+		{
+			var result = (ViewResult)controller.Create();
+
+			Assert.That(result.ViewName, Is.EqualTo(""));
+		}
+
+		[Test]
+		public void CreateGet_AllowsHttpGet()
+		{
+			var result = controller.HasAttribute("Create", typeof(HttpGetAttribute));
+
+			Assert.That(result, Is.True);
+		}
+		
+
+	}
+}
---
+++ Web/Models/SessionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+	public class SessionViewModel
+	{
+		[Required(ErrorMessage = "Must enter a username")]
+		[Display(Name = "Username")]
+		public string Username { get; set; }
+
+		[Required(ErrorMessage = "Must enter a password")]
+		public string Password { get; set; }
+	}
+}
